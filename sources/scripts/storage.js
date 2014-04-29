f58a1068581af9ed289bf42607b8ceab3558da99
Michael Reichart <michael@zenbox.de>
2014-04-29 15:27:34 +0200

    Storage

---
+++ sources/scripts/storage.js
@@ -0,0 +1,26 @@
+// Storage
+
+var storage = (function ($) {
+'use strict';
+// - - - - - - - - - -
+
+	var
+		fn = {},
+	end;
+
+	fn = {
+		onstorevalues 	: 	function () {
+								
+							},
+	};
+
+	return fn;
+
+// - - - - - - - - - -
+})(jQuery);
+
+$(document).ready(function () {
+
+	$('form').on('submit', cache.onstorevalues);
+
+});
