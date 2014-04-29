76a0d4a541162da4f46d01f5d2da32ff25f82eab
Michael Reichart <michael@zenbox.de>
2014-04-29 16:03:39 +0200

    Storage

--- sources/scripts/storage.js
+++ sources/scripts/storage.js
@@ -5,14 +5,23 @@ var storage = (function ($) {
 // - - - - - - - - - -
 
 	var
-		fn = {},
+		fn   = {},
+		data = null,
+		
+		element = '#chat [type=text]',
 	end;
 
 	fn = {
 		onstorevalues 	: 	function () {
 								event.preventDefault();
 								console.log('storage store values!');
+
+								data = $(element).val();
+								fn.storeData();
 							},
+		storeData       : 	function () {
+								localStorage.setItem('chatData', data);
+							}
 	};
 
 	return fn;
